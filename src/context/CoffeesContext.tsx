3bea7e5fde6947df3e3ef61529c14d07edb4e51e
Igor Santos <falaigors@gmail.com>
2022-08-27 13:38:29 -0300

    create data list of the coffees

--- src/context/CoffeesContext.tsx
+++ src/context/CoffeesContext.tsx
@@ -1,11 +1,18 @@
 import { createContext, ReactNode, useState } from "react";
 
 interface Coffees {
+  id: string;
   title: string;
   description: string;
   tags: string[];
   value: string;
   image: string;
+}
+
+interface CartItem {
+  id: string;
+  coffeeId: string;
+  quantity: number;
 }
 
 interface CoffeesContextData {
@@ -20,6 +27,7 @@ interface CoffeesProviderProps {
 
 export function CoffeesProvider({ children }: CoffeesProviderProps) {
   const [coffees, setCoffees] = useState<Coffees[]>([]);
+
   return (
     <CoffeesContext.Provider value={{ coffees }}>
       {children}
