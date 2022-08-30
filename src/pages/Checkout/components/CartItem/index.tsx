2382861229cd492aa8b3f23ca839f2f09dac6378
Igor Santos <falaigors@gmail.com>
2022-08-29 22:10:20 -0300

    separando cartItem em um component]

---
+++ src/pages/Checkout/components/CartItem/index.tsx
@@ -0,0 +1,45 @@
+import { Trash } from "phosphor-react";
+import { useContext } from "react";
+import { QuantityButton } from "../../../../components/QuantityButton";
+import { CartContext } from "../../../../context/CartContext";
+import { coffees } from "../../../../store/data";
+import { Actions, CartListItem, Remove } from "./styles";
+
+export function CartItem() {
+  const { cart } = useContext(CartContext);
+
+  return (
+    <div>
+      {cart.map((item) => {
+        const coffee = coffees.find((coffee) => coffee.id === item.coffeeId);
+
+        const pricing = coffee?.value.toLocaleString("pt-br", {
+          minimumFractionDigits: 2,
+        });
+
+        return (
+          <div key={item.id}>
+            <CartListItem>
+              <img src={coffee?.image} alt={coffee?.description} />
+              <div>
+                <p>{coffee?.title}</p>
+                <Actions>
+                  <QuantityButton />
+
+                  <Remove onClick={() => console.log("remove")}>
+                    <Trash size={22} /> Remover
+                  </Remove>
+                </Actions>
+              </div>
+              <p>
+                <strong>{pricing}</strong>
+              </p>
+            </CartListItem>
+
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
--- src/pages/Checkout/components/ResumeOrder/index.tsx
+++ src/pages/Checkout/components/ResumeOrder/index.tsx
@@ -1,42 +1,14 @@
-import { Trash } from "phosphor-react";
 import { ConfirmOrder } from "../ConfirmOrder";
-import { QuantityButton } from "../../../../components/QuantityButton";
 
-import {
-  Actions,
-  CartListItem,
-  Order,
-  Remove,
-  ResumeContainer,
-} from "./styled";
-
-import expresso from "../../../../assets/coffees/expresso.png";
+import { Order, ResumeContainer } from "./styled";
+import { CartItem } from "../CartItem";
 
 export function ResumeOrder() {
   return (
     <ResumeContainer>
       <h3>Cafés selecionados</h3>
       <Order>
-        <div>
-          <CartListItem>
-            <img src={expresso} alt="" />
-            <div>
-              <p>Expresso tradicional</p>
-              <Actions>
-                <QuantityButton />
-
-                <Remove onClick={() => console.log("remove")}>
-                  <Trash size={22} /> Remover
-                </Remove>
-              </Actions>
-            </div>
-            <p>
-              <strong>R$ 9,90</strong>
-            </p>
-          </CartListItem>
-
-          <hr />
-        </div>
+        <CartItem />
 
         <ConfirmOrder />
       </Order>
